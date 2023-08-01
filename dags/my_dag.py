134d4cfe65c494dd563256caddc47fd4aa497f6b
Nicholas Payne <nic.payne@avant.com>
2023-08-01 17:20:23 -0500

    uncomment runtime hook in my_dag - fails though

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -4,17 +4,19 @@ from datetime import datetime
 from airflow.operators.python import PythonOperator
 
 from airflow import DAG
+from runtime_hook import RuntimeHook
 
 
 def hello_world():
     print("Hello, World!")
-
 
 with DAG(
     dag_id="hello_world_dag",
     start_date=datetime(2021, 1, 1),
     schedule_interval="@hourly",
     catchup=False,
+    on_failure_callback=RuntimeHook().on_failure_callback,
+    on_success_callback=RuntimeHook().on_success_callback,
 ) as dag:
     task1 = PythonOperator(task_id="hello_world", python_callable=hello_world)
 

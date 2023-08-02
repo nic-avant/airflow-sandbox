487ddef61720729489a5b02dd187c2a1a2321f00
Nicholas Payne <nic.payne@avant.com>
2023-08-01 19:52:57 -0500

    reset the hook to what pagerduty hook was and comment out a few things

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -11,25 +11,81 @@ import os
 
 
 class RuntimeHook(BaseHook):
-    """Hook for runtime actions"""
+    """Hook for runtime event handling"""
 
     avant_conn_name = "avant_pagerduty"
 
     def __init__(self, *args, **kwargs):  # pylint: disable=unused-argument
         self.foo = 'foo'
+        # avant_conn = self.get_connection(self.avant_conn_name)
+        # self.avant_routing_key = avant_conn.extra_dejson.get('Password')
+        # print(avant_conn)
+        # print(type(avant_conn))
+        # print(self.avant_routing_key)
 
     def on_failure_callback(self, ctx, **kwargs):
+        ti = ctx["task_instance"]
+        run_id = ctx["run_id"]
         return self._invoke(
-            '1', '2', '3', '4', '5'
+            dag_id=ti.dag_id,
+            run_id=run_id,
+            task_id=ti.task_id,
+            log_url=ti.log_url,
+            action="trigger"
         )
 
     def on_success_callback(self, ctx, **kwargs):
+        ti = ctx["task_instance"]
+        run_id = ctx["run_id"]
         return self._invoke(
-            '1', '2', '3', '4', '5'
+            dag_id=ti.dag_id,
+            run_id=run_id,
+            task_id=ti.task_id,
+            log_url=ti.log_url,
+            action="resolve"
         )
 
     def _invoke(self, dag_id, run_id, task_id, log_url, action):
-        print('INVOKED THE RUNTIMEHOOK')
+        # severity can be critical, error, warning or info
+        if action not in ('trigger', 'resolve'):
+            raise ValueError("action must be 'trigger' or 'resolve'")
+
+        print(f'Triggering {dag_id=}')
+
+        # dedup_key = str(hash(dag_id + run_id + task_id))
+        # base_message = f' incident event in Avant PagerDuty for dag_id={dag_id} and run_id={run_id}'
+        #
+        # try:
+        #     events_session = pdpyras.EventsAPISession(self.avant_routing_key)
+        #     if action == 'trigger':
+        #         print('Triggering ' + base_message)
+        #         message = """
+        #         Avant PagerDuty: {} Airflow Campaign Partner01 DAG '{}' failed on task '{}'.
+        #         See Airflow logs for run_id='{}'
+        #         """.format(
+        #             'TEST',
+        #             dag_id,
+        #             task_id,
+        #             run_id
+        #         )
+        #         details = {
+        #             "dag": dag_id,
+        #             "task": task_id,
+        #             "run": run_id
+        #         }
+        #         events_session.trigger(summary=message,
+        #                                source='Airflow Campaign Partner01',
+        #                                severity='warning',
+        #                                dedup_key=dedup_key,
+        #                                links=[{'href': log_url}],
+        #                                custom_details=details)
+        #     else:
+        #         print('Resolving ' + base_message)
+        #         events_session.resolve(dedup_key)
+        #     print(f'Successfully executed PagerDuty action={action}')
+        #
+        # except Exception as e:
+        #     print(e)
 
 
 def hello_world():

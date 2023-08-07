868190e6bbab42166b32526071a3a7e583e49d23
Nicholas Payne <nic.payne@avant.com>
2023-08-07 17:15:27 -0500

    WIP: get started using simple function but need to figure out task vs dag level and keep it straight what I'md oing

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,109 +1,67 @@
 "hello world airflow dag"
+import time
 from datetime import datetime
-from functools import wraps
 
+import requests
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 
-from airflow import DAG
+from airflow.configuration import ConfigParser
 
-from airflow.hooks.base_hook import BaseHook
-
-from airflow.operators.empty import EmptyOperator
-import requests
+# TODO: Am I writing this for task or dag level hcio? I think DAG so the task
+# level callback is not what I need to do... just a function that takes a slug
+# to be tacked onto the end of a DAG in campaign
 
 # I think this is all that really needs to be configured...
 PROJECT_PING_KEY = "844d7hQguj_-hZ26ByMAeA"
-SLUG = "data-80123"
-URL =f"https://hc-ping.com/{PROJECT_PING_KEY}/{SLUG}"
-class RuntimeHook(BaseHook):
-    """Hook for runtime event handling"""
 
-    avant_conn_name = "avant_pagerduty"
+# TODO: remove default slug value
+def hcio_dag_alert(slug: str = 'data-80123', **kwargs):
+    """Function to be used a python_callable for DAG Healthcheck with hcio
 
-    def __init__(self, *args, **kwargs):  # pylint: disable=unused-argument
-        self.foo = 'foo'
+    Args:
+        slug: slug id of hcio endpoint to use - endpoints are to be created in avant-data-gitops repo
+        **kwargs: Airflow context
+    """
+    slug = kwargs.get('slug')
+    success = kwargs.get('success', True)
 
-    def on_failure_callback(self, ctx, **kwargs):
-        ti = ctx["task_instance"]
-        run_id = ctx["run_id"]
-        return self._invoke(
-            dag_id=ti.dag_id,
-            run_id=run_id,
-            task_id=ti.task_id,
-            log_url=ti.log_url,
-            action="trigger"
-        )
+    url =f"https://hc-ping.com/{PROJECT_PING_KEY}/{slug}"
+    requests.get(url if success is True else url + "/fail")
 
-    def on_success_callback(self, ctx, **kwargs):
-        ti = ctx["task_instance"]
-        run_id = ctx["run_id"]
-        return self._invoke(
-            dag_id=ti.dag_id,
-            run_id=run_id,
-            task_id=ti.task_id,
-            log_url=ti.log_url,
-            action="resolve"
-        )
+def hcio_task_alert(ctxt: dict):
+    """HCIO alert utility for drop-in hcio integration to Airflow tasks
 
-    def hcio_alert(self, action: str):
-        # hcio alert
-        requests.get(URL if action=='resolve' else URL + "/fail")
+    Args:
+        ctxt: Airflow context
+    Notes:
+        Creation of new HCIO endpoints is handled via GH actions in avant-data-gitops
+        # TODO: instructions for that here or in gitops repo?
+    """
 
-    def _invoke(self, dag_id, run_id, task_id, log_url, action):
-        # severity can be critical, error, warning or info
-        if action not in ('trigger', 'resolve'):
-            raise ValueError("action must be 'trigger' or 'resolve'")
+    config: ConfigParser = ctxt['config']
+    ...
 
-        self.hcio_alert(action)
-        print('Triggering a dag')
 
 
 def hello_world():
-    # raise ValueError()
     print("Hello, World!")
+    time.sleep(5)
 
-
-def hcio_decorator(slug: str):
-    """
-    Could use a decorator like this if tasks are often python functions, then
-    just decorading with the slug would be easy implementation per operator
-    """
-
-    # TODO: get this from env variable/vault/airflow config?
-    project_ping_key = "844d7hQguj_-hZ26ByMAeA"
-
-    url =f"https://hc-ping.com/{project_ping_key}/{slug}"
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            try:
-                func(*args, **kwargs)
-                requests.get(url)
-            # TODO: what to catch?
-            except Exception as e:
-                # can post arbitrary data to hcio endpoint
-                requests.post(url + "/fail", data={'error': e})
-        return wrapper
-
-    return decorator
-
-@hcio_decorator(slug='data-80123')
-def talk():
-    """talk docstring"""
-    print("Did I hcio?")
-    raise ValueError('raised ValueError to test posting data')
 
 with DAG(
     dag_id="hello_world_dag",
     start_date=datetime(2021, 1, 1),
     schedule_interval="@hourly",
     catchup=False,
-    on_failure_callback=RuntimeHook().on_failure_callback,
-    on_success_callback=RuntimeHook().on_success_callback,
+    # on_failure_callback=RuntimeHook().on_failure_callback,
+    # on_success_callback=RuntimeHook().on_success_callback,
 ) as dag:
 
     task0 = EmptyOperator(task_id="id0")
-    task1 = PythonOperator(task_id="hello_world", python_callable=hello_world)
-    task2 = PythonOperator(task_id="talk", python_callable=talk)
+    task1 = PythonOperator(task_id="hello_world", python_callable=hello_world, on_execute_callback=hcio_task_alert)
+    task2 = PythonOperator(task_id="hello_world2", python_callable=hello_world)
+    hcio = PythonOperator(task_id="hcio", python_callable=hcio_dag_alert, op_kwargs={'slug':'data-80123'})
 
     task0 >> task1 >> task2

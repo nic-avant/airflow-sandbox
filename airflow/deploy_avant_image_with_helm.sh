cf600dd607d9fed34baac8bfc41a4e408db12405
Nicholas Payne <nic.payne@avant.com>
2023-10-27 14:18:17 -0500

    try to add values to values.yml

--- airflow/deploy_avant_image_with_helm.sh
+++ airflow/deploy_avant_image_with_helm.sh
@@ -1,10 +1,10 @@
 docker pull avant.jfrog.io/shared-images/avant-airflow:dev
 helm repo add apache-airflow https://airflow.apache.org/
 helm repo update
+helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace
 kubectl apply -f ./airflow-volume.yml
 helm upgrade airflow apache-airflow/airflow \
-    --namespace default \
-    --version 1.6.0 -f ./values.yml \
-    --set images.airflow.repository=avant.jfrog.io/shared-images/avant-airflow \
-    --set images.airflow.tag=dev \
+    --namespace airflow \
+    --version 1.11.0 \
+    -f ./values.yml \
     --wait=false

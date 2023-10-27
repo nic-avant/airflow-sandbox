f3b9fe32fdf6586a63b39c2f84f12b859aa36aec
Nicholas Payne <nic.payne@avant.com>
2023-10-27 15:29:30 -0500

    wip

--- airflow/deploy_avant_image_with_helm.sh
+++ airflow/deploy_avant_image_with_helm.sh
@@ -1,10 +1,11 @@
-docker pull avant.jfrog.io/shared-images/avant-airflow:dev
-helm repo add apache-airflow https://airflow.apache.org/
-helm repo update
-helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace
-kubectl apply -f ./airflow-volume.yml
+# docker pull avant.jfrog.io/shared-images/avant-airflow:dev
+# helm repo add apache-airflow https://airflow.apache.org/
+# helm repo update
+# helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace
+# kubectl apply -f ./airflow-volume.yml
 helm upgrade airflow apache-airflow/airflow \
     --namespace airflow \
     --version 1.11.0 \
-    -f ./values.yml \
+    -f values.yml \
     --wait=false
+# --set dagProcessor.enabled=true \

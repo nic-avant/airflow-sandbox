38492e16d4a24ad8640112474a19bed034bcba73
Nicholas Payne <nic.payne@avant.com>
2023-11-08 20:42:44 -0600

    USE airflow namespace appropriately!!!

--- airflow/deploy_avant_image_with_helm.sh
+++ airflow/deploy_avant_image_with_helm.sh
@@ -1,11 +1,18 @@
 # docker pull avant.jfrog.io/shared-images/avant-airflow:dev
-# helm repo add apache-airflow https://airflow.apache.org/
-# helm repo update
-# helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace
-# kubectl apply -f ./airflow-volume.yml
-helm upgrade airflow apache-airflow/airflow \
+helm repo add apache-airflow https://airflow.apache.org/
+helm repo update
+kubectl create namespace airflow
+kubectl apply -f ./airflow-volume.yml
+# helm install airflow apache-airflow/airflow --version 1.6.0 -f values.yaml
+helm upgrade \
+    --install airflow apache-airflow/airflow \
     --namespace airflow \
     --version 1.11.0 \
-    -f values.yml \
-    --wait=false
-# --set dagProcessor.enabled=true \
+    -f values.yaml \
+    --set images.airflow.repository=avant.jfrog.io/shared-images/avant-airflow \
+    --set images.airflow.tag=dev \
+    --set dagProcessor.enabled=true \
+    --set webserver.replicas=1 \
+    --set triggerer.replicas=1 \
+    --set scheduler.replicas=1 \
+    --wait=true

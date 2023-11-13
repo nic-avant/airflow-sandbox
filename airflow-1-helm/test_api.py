c8e8bdeebaf221a1ca7b3bb8358b365831185967
Nicholas Payne <nic.payne@avant.com>
2023-11-13 13:58:04 -0600

    wip

--- airflow-1-helm/test_api.py
+++ airflow-1-helm/test_api.py
@@ -1,11 +1,18 @@
 import logging
+import time
 from datetime import datetime
+from functools import wraps
 from typing import List
 
 import requests
 from pydantic import BaseModel
+from rich.logging import RichHandler
+from rich.prompt import Prompt
 
-logging.basicConfig(level=logging.INFO)
+# Setup logging with Rich
+logging.basicConfig(
+    level=logging.INFO, format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
+)
 
 headers = {"Cache-Control": "no-cache", "Content-Type": "application/json"}
 AIRFLOW_URL = "http://localhost:8081"
@@ -27,6 +34,17 @@ class DagPausedStatus(BaseModel):
     dag_id: str
 
 
+def sleep_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        time.sleep(1)
+        return result
+
+    return wrapper
+
+
+@sleep_decorator
 def get_dags():
     logging.info("Fetching DAGs...")
     response = requests.get(
@@ -36,30 +54,36 @@ def get_dags():
     return LatestRunsResponse(**response.json()).items
 
 
+@sleep_decorator
 def get_dag_status(dag_id):
     logging.info(f"Fetching status for DAG: {dag_id}")
     response = requests.get(
         f"{AIRFLOW_URL}/api/experimental/dags/{dag_id}/paused", headers=headers
     )
     response.raise_for_status()
+    logging.info(f"DAG Paused: {response.json()['is_paused']}")
     return DagPausedStatus(is_paused=response.json()["is_paused"], dag_id=dag_id)
 
 
+@sleep_decorator
 def pause_dag(dag_id):
     logging.info(f"Pausing DAG: {dag_id}")
     response = requests.get(
         f"{AIRFLOW_URL}/api/experimental/dags/{dag_id}/paused/true", headers=headers
     )
     response.raise_for_status()
+    logging.info(f"{response.json()}")
     return response.json()
 
 
+@sleep_decorator
 def unpause_dag(dag_id):
     logging.info(f"Unpausing DAG: {dag_id}")
     response = requests.get(
         f"{AIRFLOW_URL}/api/experimental/dags/{dag_id}/paused/false", headers=headers
     )
     response.raise_for_status()
+    logging.info(f"{response.json()}")
     return response.json()
 
 
@@ -71,7 +95,7 @@ def main():
     for dag in not_paused:
         pause_dag(dag.dag_id)
 
-    input("Press enter to unpause dags")
+    Prompt.ask("Press enter to unpause dags")
 
     for dag in not_paused:
         unpause_dag(dag.dag_id)
